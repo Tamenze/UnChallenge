2097ccb34236fcb921f0325cf04d0c97360edd8e
Tolulope Edionwe <tamenze@gmail.com>
2016-05-19 07:09:28 -0400

    Completed all crud ajax conversion, now styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,13 +2,16 @@ class TasksController < ApplicationController
 #add todo item
 #mark them as done ( right now they default to not done, i need to:
 # 				make them display done vs not done (instead of true/false)
-
-
-#delete todo item
-# =>  			make the checkbox render next to each one in the list (so allow update from index view)
-
+# delete todo item
+# add in create form to index page (render?)
+#make actions ajax (so create, update, and delete)
+# => make the checkbox render next to each one in the list (so allow update from index view)
+#clear text field after creating task
+#styling
 	def index
 		@tasks = Task.all
+		@task = Task.new
+		# render 'new'
 	end
 
 	def new
@@ -17,18 +20,22 @@ class TasksController < ApplicationController
 
 	def create
 		@task = Task.create(task_params)
-		if @task.save 
-			flash[:notice] = "Task successfully added!"
-			redirect_to tasks_path
-		else
-			flash[:alert] = "Problem saving task"
-			redirect_to new_task_path
+		respond_to do |format|
+			if @task.save 
+				# format.html {redirect_to @task, notice: "Task successfully added!"}
+				# format.json{ render json: @task, status: :created, location: @task}
+				format.js
+			else
+				format.html { render action: "new"}
+				format.json{ render json: @task.errors, status: unprocessable_entity}
+				format.js
+			end
 		end
 	end
 
-	def show
-		@task = Task.find(params[:id])
-	end
+	# def show
+	# 	@task = Task.find(params[:id])
+	# end
 
 
 	def edit
@@ -37,16 +44,33 @@ class TasksController < ApplicationController
 
 	def update
 		@task = Task.find(params[:id])
-		if @task.update(task_params)
-			flash[:notice] = "Task successfully updated!"
-			redirect_to tasks_path
-		else
-			flash[:alert] = "Problem saving task"
-			redirect_to edit_task_path
+		@task.update(task_params)
+		
+		respond_to do |format|
+			if @task.update(task_params)
+				# flash[:notice] = "Task status successfully updated!"
+				# redirect_to tasks_path
+				# fornat.html{}
+				format.js
+			else
+				# flash[:alert] = "Problem updating task status."
+				# redirect_to edit_task_path
+				format.js
+			end
 		end
 	end
 
 	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy
+		respond_to do |format|
+			if @task.destroy
+				format.html {redirect_to tasks_path, notice: "Deleted Task successfully."}
+				format.js
+			else
+			format.html{ redirect_to tasks_path notice: "NOT DELETED." }
+			end
+		end
 	end
 
 
